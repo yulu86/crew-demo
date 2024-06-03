94e19673616c9e939c90cbec8a8eaa4edf88e4d5
Yulu Xu <xuyulu86@126.com>
2024-06-03 22:28:34 +0800

    update ollama

--- src/01_ollama.py
+++ src/01_ollama.py
@@ -1,19 +1,17 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama
 from crewai import Agent, Task, Crew
 import os
 
 os.environ["OPENAI_API_KEY"] = "NA"
 
-llm = ChatOpenAI(
-    model = "llama3:instruct",
-    base_url = "http://localhost:11434/v1")
+ollama_model = Ollama(model = "llama3:instruct")
 
 general_agent = Agent(role = "Math Professor",
                       goal = """Provide the solution to the students that are asking mathematical questions and give them the answer.""",
                       backstory = """You are an excellent math professor that likes to solve math questions in a way that everyone can understand your solution""",
                       allow_delegation = False,
                       verbose = True,
-                      llm = llm)
+                      llm = ollama_model)
 task = Task (description="""what is 3 + 5""",
              agent = general_agent,
              expected_output="A numerical answer.")
